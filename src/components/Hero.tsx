7212f1bd39023140969d4ece465edd1308b082d9
akanepajs <40496475+akanepajs@users.noreply.github.com>
2025-12-23 07:16:18 +0200

    Update Hero component text and formatting

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,7 +37,9 @@ const Hero = () => {
           >
             <span className="text-foreground">Arturs</span>
             <br />
-            <span className="text-gradient">Kanepajs 🔸</span>
+            <span className="text-gradient">Kanepajs</span>
+            <br />
+            <span className="text-foreground">🔸</span>
           </h1>
 
           {/* Title */}
