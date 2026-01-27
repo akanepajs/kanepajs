8da3727c16d3a3aee0af29f02329fe0c8aa70b9c
akanepajs <40496475+akanepajs@users.noreply.github.com>
2026-01-27 10:52:42 +0200

    Remove emoji from 10% pledger tag

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -61,7 +61,6 @@ const Hero = () => {
               "Economic Policy",
               "Animal Welfare",
               "Effective Giving",
-              "🔸10% pledger #3672",
               "10% pledger #3672"
             ].map((tag) => (
               <span
